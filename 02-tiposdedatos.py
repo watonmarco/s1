80f99335f5d99d5a7275c22ea8213e08afe5a0f9
watonmarco <watonmarco@users.noreply.github.com>
2023-04-06 20:54:36 -0400

    primer commit, hola

---
+++ 02-tiposdedatos.py
@@ -0,0 +1,32 @@
+#1. datos de tipo numérico
+estatura = 1.71
+peso = 70
+complejo = 1 + 4j
+
+print("Impresión del número complejo:",complejo)
+
+##operación aritmética básica
+imc = peso/estatura
+print("mi imc es de:",imc)
+
+#2. datos de tipo cadena de caracteres
+asignatura = "Programación"
+carrera = "Ingeniería Civil en Informática"
+
+##hola (saludo)
+print("Hola, soy estudiante de la carrera de",carrera,"y estoy en clase de",asignatura+", un (1) cordial saludo,,")
+
+#3. valores booleanos (no se que es un booleano)
+ampolleta = False
+interruptor = True
+
+print(type(ampolleta))
+print(type(imc))
+print(type(carrera))
+print(type(peso))
+
+#4. datos de tipo array (o arreglos)
+estudiantes = ("Matías", "Marco", "Cristóbal", "Sebastian")
+num = (1,2,3,4,5,6)
+print(estudiantes)
+print(num)
---
+++ no-hay-internet-ayuda.py
@@ -0,0 +1,6 @@
+#no hay internet ayuda
+print("no hay internet ayuda")
+
+##todos trabajando con git y yo sin internet jejjej
+
+############## me conecté a internet pero hubo problemas con los permisos y no pude subir esto xd
